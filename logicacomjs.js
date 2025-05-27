bbb77e59bd86a6e1fb4ba5131c4e2129bc4ddd20
Maraisa451823 <maraisavitoria78@gmail.com>
2025-05-27 16:21:25 -0300

    aprendi if e else

--- logicacomjs.js
+++ logicacomjs.js
@@ -18,14 +18,49 @@ let crechê = 50
 let crianças =30
 let entregadepipocas=crechê/crianças
 console.log("A divisão é :"+entregadepipocas+"R$")
-console.log("-----------------------")*/
-
+console.log("-----------------------")
 let salário= number(prompot("2500"));
 let mesestrab= number(prompot("5"));
 let totalanual= wesllainy/12*mesestrab
 console.log("Seu décimo+ é: " +décimo)
+let x= number(prompt("Digite um número"))
+console.log(x)
+let salário= number(prompot("Digite seu salário"));
 
+if (salario<500){ 
+let salarioatual=("15/100*salario+salario")
+alert("Seu novo salário é:"+salario)
+} 
+elseif(salario>=500 && salário<=1000){ 
+ salarioatual=(10/100*salario+salario)
+alert ("Seu novo salario é: " +salario)
+}{
+    salario("5/100*salario+salario")
+    alert ("Seu novo salario é: " +salario)
+}*/
 
+let idade = Number(prompot("digite sua idade:"))
+ if (idade<=12 && idade<=17){ 
+  alert ("Seu conteúdo é infantil")
+} 
+else if(idade>=18 && idade<=60){
+    alert("Seu conteúdo é teen")
+}
+else{
+    alert("Seu conteúdo é Sênior")
+}
 
-/*let x= number(prompt("Digite um número"))
-console.log(x)*/
+let pontuacao = Number(prompot("digite sua pontuação:"))
+if ( pontuacao <=1000){ 
+    alert ("Seu nivel é novato e o seu bõnus XP é de 100XP")
+}
+else if(pontuacao>=1001 && pontuacao<=5000){
+    alert("Seu nivel é aprendiz e o seu bônus XP é de 500XP")
+}
+else if ( pontuacao>=5001 && pontuacao<=10000){
+    alert("Seu nivel é mestre e o seu bônus é de 1000PX")
+ }
+ else { 
+    alert("Seu nivel é mestre e o seu bônus é de 2000PX")
+ }
+   
